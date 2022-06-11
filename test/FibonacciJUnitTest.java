9d2b998d9c39ea52b751849b663d33f60f540944
VMoises <moisesvilladiegos@gmail.com>
2022-06-11 16:26:42 -0500

    Se escribe la prueba

---
+++ test/FibonacciJUnitTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import fibonacci.Fibonacci;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author moise
+ */
+public class FibonacciJUnitTest {
+    
+    Fibonacci fibonacci = new Fibonacci();
+    
+    @Test
+    public void testFibonacci1(){
+        long esperado = 0L;
+        long resultado = fibonacci.fibonacci(1);
+        assertEquals(0, resultado);
+    }
+}
